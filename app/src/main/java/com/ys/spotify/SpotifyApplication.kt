a779da31ed8b209c6fc5d94a7e4c54ea16857b09
YS <yoonsam86@gmail.com>
2022-04-08 16:10:55 +0900

    Dagger-Hilt Setup

---
+++ app/src/main/java/com/ys/spotify/SpotifyApplication.kt
@@ -0,0 +1,7 @@
+package com.ys.spotify
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SpotifyApplication : Application()
---
+++ app/src/main/java/com/ys/spotify/data/di/ServiceModule.kt
@@ -0,0 +1,48 @@
+package com.ys.spotify.data.di
+
+import android.content.Context
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.upstream.DefaultDataSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+    fun provideAudioAttributes() = AudioAttributes.Builder()
+        .setContentType(C.CONTENT_TYPE_MUSIC)
+        .setUsage(C.USAGE_MEDIA)
+        .build()
+
+    @ServiceScoped
+    @Provides
+    fun provideExoPlayer(
+        @ApplicationContext context: Context,
+        audioAttributes: AudioAttributes
+    ) = ExoPlayer.Builder(context).apply {
+        setAudioAttributes(audioAttributes, true)
+        // 헤드폰, 이어폰등을 연결하면 매우 시끄럽기 때문에 일시정지하는 기능
+        setHandleAudioBecomingNoisy(true)
+    }
+
+    @ServiceScoped
+    @Provides
+    fun provideDataSourceFactory(
+        @ApplicationContext context: Context
+    ) = DefaultDataSource.Factory(
+        context,
+        DefaultHttpDataSource
+            .Factory()
+            .setUserAgent("Spotify App")
+    )
+}
